b74223efe83c5aa33358368d8db138276f226563
pei-1102 <peityann@gmail.com>
2025-01-13 19:21:25 +0800

    update 作業

---
+++ Assets/Practice/Practice_1_Variable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Practice_1_Variable
+{
+    public int price = 999;
+    private float jumpHeight = 77.5f;
+    public string weapon = "三項之力";
+    private bool getDragonSoul = true
+}
+
---
+++ Assets/Practice/Practice_2_.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Practice_2_ : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
