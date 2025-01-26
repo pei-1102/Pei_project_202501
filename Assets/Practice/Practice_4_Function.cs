1bb6b3693bab453090e374e8b1542ab0c85cdd28
pei-1102 <peityann@gmail.com>
2025-01-26 20:31:25 +0800

    update 課程4 _作業

---
+++ Assets/Practice/Practice_4_Function.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Practice_4_Function : MonoBehaviour
+{
+    private void Awake()
+    {
+    
+        Debug.Log($"<color= #3f3>布林值 True :{ReturnTrue()}</color>");
+        Debug.Log($"<color= #3f3>布林值 False :{ReturnFalse()}</color>");
+    }
+
+
+    /// <summary>
+    /// 布林值 True
+    /// </summary>
+    /// <returns>True</returns>
+    private bool ReturnTrue()
+    {
+        return true;
+    
+    }
+    /// <summary>
+    /// 布林值 False
+    /// </summary>
+    /// <returns></returns>
+     private bool ReturnFalse()
+    {
+        return false;
+    
+    }
+
+}
