c32b3244da9e0187361ad897c761f71f00c0f22b
pei-1102 <peityann@gmail.com>
2025-01-26 19:51:49 +0800

    update 練習_課程三

---
+++ Assets/Practice/Practice_3_Operator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Practice_3_Operator : MonoBehaviour
+{
+    private void Awake()
+    {
+        // 計算 6/2(1+2)
+        
+        int answer = 6 / 2 * (1 + 2);
+        Debug.Log($"<color=:#f33>6/ 2 * ( 1 + 2) = {answer}<color>");
+
+    }
+
+
+}
